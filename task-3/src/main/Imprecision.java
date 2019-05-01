39dca84c61cb85e62c3029aa7eec31da7b358ebf
Janik Steegmüller <me@jsteegmueller.net>
2019-05-01 13:21:07 +0200

    task 2.2 wtf

--- task-3/src/main/Imprecision.java
+++ task-3/src/main/Imprecision.java
@@ -10,9 +10,9 @@ public class Imprecision {
         float forward_sum = 0;
         float backward_sum = 0;
 
-        for(int i = 1 ; i <= 100 ; i++){
-            forward_sum += ( (Math.pow(-1,i+1) / i) * Math.pow(x-1, i) );
-            backward_sum +=  ( (Math.pow(-1,(100 - i)+1) / (100 - i)) * Math.pow(x-1, (100 - i)) );
+        for(float i = 1 ; i <= 100 ; i++){
+            forward_sum +=  ((float)Math.pow(x-1.f, i) * (float)Math.pow(-1.f,i+1.f)) / i;
+            backward_sum += ((float)Math.pow(x-1.f, (101.f - i)) * (float)Math.pow(-1.f,(101.f - i)+1.f)) / (101.f - i);
         }
 
         return forward_sum - backward_sum;
@@ -23,7 +23,10 @@ public class Imprecision {
     public static double double_eval_polynom(double x0, double y0){
         double res = 0;
 
-        res = 333.75 * Math.pow(y0,6) + Math.pow(x0, 2) * (11 * Math.pow(x0, 2) * Math.pow(y0, 2) - Math.pow(y0,6) - 121 * Math.pow(y0,4) -2) + 5.5 * Math.pow(y0,8) + x0/(2*y0);
+        res = (333.75 * Math.pow(y0,6)) +
+                (Math.pow(x0,2) * ((11 * Math.pow(x0,2) * Math.pow(y0,2)) - Math.pow(y0,6) - (121 * Math.pow(y0, 4)) - 2)) +
+                (5.5*Math.pow(y0,8)) +
+                (x0 / (2*y0));
         return res;
     }
 
@@ -32,7 +35,11 @@ public class Imprecision {
     public static float float_eval_polynom(float x0, float y0){
         float res = 0;
 
-        res = 333.75f * (float)Math.pow(y0,6) + (float)Math.pow(x0, 2) * (11f * (float)Math.pow(x0, 2) * (float)Math.pow(y0, 2) - (float)Math.pow(y0,6) - 121f * (float)Math.pow(y0,4) - 2f) + 5.5f * (float)Math.pow(y0,8) + x0/(2f*y0);
+        res = (333.75f * (float)Math.pow(y0,6)) +
+                ((float)Math.pow(x0,2f) * ((11f * (float)Math.pow(x0,2) * (float)Math.pow(y0,2f)) - (float)Math.pow(y0,6f) - (121f * (float)Math.pow(y0, 4f)) - 2f)) +
+                (5.5f*(float)Math.pow(y0,8f)) +
+                (x0 / (2f*y0));
+
 
 
         return res;
@@ -46,7 +53,7 @@ public class Imprecision {
         System.out.println(different_same_sum(2.97f));
         System.out.println("2.2 b) ");
         System.out.println(double_eval_polynom(77617, 33096));
-        System.out.println(float_eval_polynom(77617, 33096));
+        System.out.println(float_eval_polynom(77617f, 33096f));
 
     }
 }
