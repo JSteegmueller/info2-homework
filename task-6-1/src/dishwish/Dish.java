f1c3aea35c48bf4fb2d1e232e384381a17f9d984
Janik Steegmüller <me@jsteegmueller.net>
2019-05-27 22:29:44 +0200

    yay a dishwasher

---
+++ task-6-1/src/dishwish/Dish.java
@@ -0,0 +1,60 @@
+package dishwish;
+
+/**
+ * Ja also das hier ist halt die Klasse wie sie in der Aufgabe beschrieben ist,
+ * Dish hat:
+ * - Name name
+ * - Schmutzgrad dirty
+ * - größe Size mit get-Methode
+ * - Top Konstruktor Dish
+ * - Sexy Methoden wie printStatus die den Status des Dishes printed (deshalb void) und
+ * - clean welches den ganzen Schmutz von dem dish entfernt (auch void) mit nem Parameter wie arg die Spühlmaschiene halt so abgeht.
+ **/
+
+public class Dish {
+    private String name;
+    private float dirty;
+    private int size;
+
+    public Dish(String name, float dirty) {
+        this.name = name;
+        determineSize(name);
+
+        if (dirty >= 0 && dirty <= 1) {
+            this.dirty = dirty;
+        } else {
+            this.dirty = 1f;
+        }
+    }
+
+    private void determineSize(String name) {
+        if (name.equals("Cutlery")) {
+            size = 1;
+        } else if (name.equals("Plate")) {
+            size = 2;
+        } else if (name.equals("Cup")) {
+            size = 2;
+        } else if (name.equals("Pot")) {
+            size = 5;
+        } else {
+            //Wir gehen mal vom schlimmsten aus, nicht dass unsere gute teure Spühlmaschiene noch überlastet wird ;P
+            size = Integer.MAX_VALUE;
+        }
+    }
+
+    public void clean(float amountOfClean) {
+        dirty -= amountOfClean;
+        if (dirty < 0) dirty = 0;
+    }
+
+    public void printStatus() {
+        System.out.println("Name: " + name);
+        System.out.println(dirty);
+        System.out.println((dirty <= 0.1) ? "is clean" : "still dirty");
+        System.out.println();
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
---
+++ task-6-1/src/dishwish/Dishwasher.java
@@ -0,0 +1,38 @@
+package dishwish;
+
+public class Dishwasher {
+
+    private static Dish[] dishes;
+    private static Dish[] inMachine;
+    private static final int maxCapacity = 20;
+    private static int capacity;
+
+    public static void main(String[] args) {
+        dishes = new Dish[10];
+        inMachine = new Dish[10];
+        capacity = 0;
+
+        dishes[0] = new Dish("Cutlery", 0.1f);
+        dishes[1] = new Dish("Cutlery", 0.1f);
+        dishes[2] = new Dish("Plate", 0.6f);
+        dishes[3] = new Dish("Plate", 0.2f);
+        dishes[4] = new Dish("Cup", 0.2f);
+        dishes[5] = new Dish("Cup", 0.8f);
+        dishes[6] = new Dish("Plate", 0.1f);
+        dishes[7] = new Dish("Pot", 0.9f);
+        dishes[8] = new Dish("Pot", 0.4f);
+        dishes[9] = new Dish("Plate", 0.4f);
+
+        int idx = 0;
+        while (idx < dishes.length && capacity + dishes[idx].getSize() <= maxCapacity) {
+            inMachine[idx] = dishes[idx];
+            capacity += dishes[idx].getSize();
+            idx++;
+        }
+
+        for (int i = 0 ; i < idx ; i++){
+            inMachine[i].clean(0.3f);
+            inMachine[i].printStatus();
+        }
+    }
+}
